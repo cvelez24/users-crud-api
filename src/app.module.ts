ec2279ab2916faeaef09c6c93375dae33bdcdb52
Carlos Mario Velez Foronda <cvelez@PCX-FVFK52SH1WFV.local>
2024-04-17 13:46:31 -0500

    enviroment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
+
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     UserModule,
-    MongooseModule.forRoot('mongodb+srv://cvelez:users1234@cluster0.yelvcoj.mongodb.net/test?retryWrites=true&w=majority&appName=Cluster0'),
+    MongooseModule.forRoot(`mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASS}@${process.env.MONGODB_CLUSTER}.yelvcoj.mongodb.net/${process.env.MONGODB_DATABASE}?retryWrites=true&w=majority&appName=Cluster0`),
 
   ],
 })
